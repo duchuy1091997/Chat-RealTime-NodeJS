18fd7ee3b30c77ec6d1a42c81a38d1d0a96852c2
duchuy1091997 <duchuy1091997@gmail.com>
2018-11-08 01:20:03 +0700

    sasas

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
 const express = require("express");
 const app = express();
-//Respone to customer something customer able to see
+
 app.use(express.static("public"));
-//Set view use ejs library views stored in views directory 
 app.set("view engine", "ejs");
 app.set("views", "./views");
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-//Server is Listening
-io.on("connection", (socket) => {
-    console.log("Có người connect!!!" + socket.id);
 
-    //Check who disconnected
-    socket.on("disconnect", () => {
-        console.log(socket.id + " Disconneted!!!");
-        
-    });
-});
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
 server.listen(3000);
 
+io.on("connection", (socket) => {
+    console.log("co nguoi ket noi " + socket.id);
+
+    //User Register
+    var usersArray = ["abc"];
+    socket.on("client-send-username", (data) => {
+        console.log(data);
+
+        //User Regiter condition
+        if(usersArray.indexOf(data) >= 0)
+        {
+            //Fail
+            socket.emit("register-fail");
+        }
+        else
+        {
+            //Success
+            usersArray.push(data);
+            console.log(usersArray);
+            //create user name for every socket id
+            socket.UserName = data;
+            socket.emit("success-register", data);
+            io.sockets.emit("user-list", usersArray);
+        }
+    });
+
+    //User Logout
+    socket.on("logout", (data) => {
+        usersArray.splice(
+            usersArray.indexOf(socket.UserName), 1
+        );
+        socket.broadcast.emit("user-list", usersArray);
+    });
+
+    //user send message
+    socket.on("user-send-message", (data) => {
+        io.sockets.emit("server-send-message", {username: socket.UserName, content: data})
+    });
+
+    //user is typing
+    socket.on("user-typing", () => {
+        var s = socket.UserName + " is typing";
+        io.sockets.emit("typing",s);
+    });
+    //stop typing
+    socket.on("user-stop-typing", () => {
+        io.sockets.emit("stop");
+    });
+});
 app.get("/", (req, res) => {
-    res.render("HomePage");
-})
+    res.render("HomePage.ejs");
+});
